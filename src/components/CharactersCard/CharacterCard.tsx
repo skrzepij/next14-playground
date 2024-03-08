dd07f5382e4fb8069cb88542eee5b71a3df843f2
Milek <milek@Mileks-MacBook-Pro.local>
2024-03-09 00:54:15 +0100

    fix

--- src/components/CharactersCard/CharacterCard.tsx
+++ src/components/CharactersCard/CharacterCard.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { Card, Inset, Heading } from '@radix-ui/themes';
-import Image from 'next/image';
-import styles from './CharacterCard.module.scss';
-import Link from 'next/link';
+import { Card, Inset, Heading } from "@radix-ui/themes";
+import Image from "next/image";
+import styles from "./CharacterCard.module.scss";
+import Link from "next/link";
 
 type CharacterCardProps = {
   imageSrc: string;
@@ -15,21 +15,14 @@ export const CharacterCard = ({ imageSrc, name, id }: CharacterCardProps) => {
   return (
     <Link href={`/characters/${id}`}>
       <Card size="2" className={styles.card}>
-        <Inset
-          clip="border-box"
-          side="top"
-          pb="current"
-          className={styles.inset}
-        >
-          <Image
-            alt="naruto character"
-            src={imageSrc}
-            quality={100}
-            fill
-            sizes="(max-width:200px)"
-            className={styles.image}
-          />
-        </Inset>
+        <Image
+          alt="naruto character"
+          src={imageSrc}
+          quality={100}
+          fill
+          sizes="(max-width:200px)"
+          className={styles.image}
+        />
         <Heading as="h2">{name}</Heading>
       </Card>
     </Link>
